60bc248eadfa279c5655e45964eb94c87103ad8c
AaronPereira21 <115249531+AaronPereira21@users.noreply.github.com>
2023-04-13 19:55:18 +0530

    Backend Connected

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def index():
 @app.route('/', methods=['POST'])
 def get_weather():
     city = request.form['city']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=54a4215d65eb60a2a0c49c14c289a227'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=54a4215d65eb60a2a0c49c14c289a227&units=metric'
     response = requests.get(url).json()
     weather = {
         'city': city,
         'temperature': response['main']['temp'],
         'description': response['weather'][0]['description'],
+        'wind_speed': response['wind']['speed'],
         'icon': response['weather'][0]['icon']
     }
     return render_template('homepage.html', weather=weather)
